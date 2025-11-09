17b1d191f48383ca30a223584a5f53a8624a88c7
Carlo Duminuco Del Monte <c.duminucodelmonte@teamsystem.com>
2025-11-09 09:25:24 +0100

    Migrazione moduli

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -4,8 +4,6 @@ import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [CurrencyPipe], // Configurata in euro (HTML)
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css',
 })
